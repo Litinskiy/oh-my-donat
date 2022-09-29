b9d47b292f9d681d4530213dae6dbbba07685c1a
Katya-Kondratchuk <ekaterina.kondratchuk@gmail.com>
2022-09-30 00:05:29 +0300

    add slider

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $('.slider').slick({
+    arrows: true,
+    dots: true,
+    slidesToShow: 3,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 800,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
