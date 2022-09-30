8fcf11f08c647792f1eb3c013ab7513722106b48
Katya-Kondratchuk <111919146+Katya-Kondratchuk@users.noreply.github.com>
2022-09-30 09:32:17 +0000

    Deploying to gh-pages from @ Katya-Kondratchuk/oh-my-donat@f05658f9823a981acdee82e812540256b2f4343a 🚀

---
+++ index.83cba88c.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick()}));
+//# sourceMappingURL=index.83cba88c.js.map
---
+++ index.a47480f0.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick()}));
+//# sourceMappingURL=index.a47480f0.js.map
