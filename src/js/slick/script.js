928f4b10f0a457d8b77aa3d28938978bf6a80920
Katya-Kondratchuk <ekaterina.kondratchuk@gmail.com>
2022-09-30 12:22:05 +0300

    add folder for img

--- src/js/slick/script.js
+++ src/js/slick/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
-  $('.slider').slick();
+  $('.slider')
+    .slick
+    //     {
+    //     asNavFor: '.reviews__content-slider',
+    //     slidesToShow: 3,
+    //     centerMode: true,
+    //     centerPadding: '20px',
+    //     responsive: [
+    //       {
+    //         breakpoint: 767,
+    //         settings: {
+    //           slidesToShow: 1,
+    //         },
+    //       },
+    //     ],
+    //   }
+    ();
 });
