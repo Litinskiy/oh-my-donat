c843015c973a58df123e892cdd6fd5357b700159
Katya-Kondratchuk <111919146+Katya-Kondratchuk@users.noreply.github.com>
2022-09-30 10:36:24 +0000

    Deploying to gh-pages from @ Katya-Kondratchuk/oh-my-donat@e316bb1eb5853f6397b9d5ae65330e1dea676534 🚀

---
+++ index.f5accc78.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),c=document.querySelector("[data-menu-close]");e.addEventListener("click",(()=>{t.classList.toggle("is-open")})),c.addEventListener("click",(()=>{t.classList.toggle("is-open")}))})();
+//# sourceMappingURL=index.f5accc78.js.map
