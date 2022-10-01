00b1dad1797e7cb5e1d5845a918e43e9f5b8641d
Katya-Kondratchuk <111919146+Katya-Kondratchuk@users.noreply.github.com>
2022-10-01 23:48:02 +0000

    Deploying to gh-pages from @ Katya-Kondratchuk/oh-my-donat@2aa65b036d5e2440c8bd9cab001bde93407cd2f3 🚀

---
+++ index.b49635c2.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),s=document.querySelector("[data-menu-close]");e.addEventListener("click",(s=>{t.classList.remove("is-hidden"),e.classList.toggle("is-hidden")})),s.addEventListener("click",(()=>{t.classList.toggle("is-hidden"),e.classList.remove("is-hidden")}))})();
+//# sourceMappingURL=index.b49635c2.js.map
