25edeffbb3edb643e65c10140c017606fa052a35
Katya-Kondratchuk <111919146+Katya-Kondratchuk@users.noreply.github.com>
2022-10-02 23:40:46 +0000

    Deploying to gh-pages from @ Katya-Kondratchuk/oh-my-donat@c08c2388c1c29899841c211a1f50c86d118f2f0c 🚀

---
+++ index.fb042e86.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick({arrows:!0,dots:!1,slidesToShow:3,autoplay:!0,centerPadding:"10px",centerMode:!0,speed:1e3,autoplaySpeed:800,responsive:[{breakpoint:1279,settings:{centerMode:!0,centerPadding:"10px",slidesToShow:3}},{breakpoint:767,settings:{slidesToShow:1,centerPadding:"10px",centerMode:!0}}]})}));var e=$(".pagingInfo");$(".slider").on("init reInit afterChange",(function(n,t,o,i){var d=(o||0)+1;e.text(d+"/"+t.slideCount)}));
+//# sourceMappingURL=index.fb042e86.js.map
